c074135d0a692bd9b8aa38097830f76961721628
Syeda Uzma <smu3@sfu.ca>
2021-06-27 18:06:11 -0700

    Implemented the write output

--- writeOutput.c
+++ writeOutput.c
@@ -1,4 +1,7 @@
-// read takes input from they keyboard and adds it to the list
+// using part of Darrn's check + addrino info since its more accurate
+
+
+
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,60 +16,93 @@
 
 #include "list.h"
 
+// Max size of the message, theoretical max size of a UDP packet in IPv4.
+#define MAXBUFLEN 65508
 
-#define PORT 22110
-#define MAX_LENGTH 1024
+// variable set up
+static int sockRead;
+static struct addrinfo hints, *servinfo, *p;
+static int gaiVal;
+static int bindVal;
+static int readBytes;
+char buf[MAXBUFLEN];
+static struct sockaddr_in their_addr;
+static socklen_t messageLen;
+static char s[INET_ADDRSTRLEN];
+char* message;
 
-char messageBuffer[MAX_LENGTH]; // buffer to store the message data
-int bytesOfMesssage;
+void receiverInit(char* hostname, char* port, List* list)
+{
 
-// creating a list
-static List* messageList = List_create();
+   //set up
+    memset(&hints, 0 ,sizeof (hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_DGRAM;
+    hints.ai_flags = AI_PASSIVE;
 
-void readInit(char* hostname, char* port, List* list){
+    //using Darren's non hard coded values
+    gaiVal = getaddrinfo(hostname, port, &hints, &servinfo);
 
-// setting up socket for reading input - not sure if need...
-    struct sockaddr_in server; 
-    memset(&server,0, sizeof(server)); // clear the socket
-    server.sin_family = AF_INET; 
-    server.sin_addr.s_addr = htonl(INADDR_ANY); // in address any, willing to work on any local port, 
-                            //dont care about port sending to
-    server.sin_port = htons(PORT); // port to connect to 
-
-
-    // creating socket// 
-    int mysocket = socket(PF_INET, SOCK_DGRAM, 0); 
-        //before was AF  instead PF -- domain = AF-> allows communication and binding so it can talk to another port
-        // type = DGRAM -> connectionless socket,for UDP, no need to maintain open connection
-
-    // binding socket //
-    int bindval= bind(mysocket, (struct sockaddr *)&server, sizeof(server));
-
-
-    while(1){
-
-    // recieve data// 
-        struct sockaddr_in messageFrom; // lets us know who we got message from, brian had sinRemote
-        unsigned int messageLen = sizeof(messageFrom); // the length
-        bytesOfMesssage = recvfrom(mysocket, messageBuffer, MAX_LENGTH, 0, (struct sockaddr *)&messageFrom,&messageLen); // main function to recieve
-
-        int interminateNull = (bytesOfMesssage < MAX_LENGTH) ? bytesOfMesssage : MAX_LENGTH -1; 
-        messageBuffer[interminateNull] = 0; 
-        printf("message recived successfully by: %d bytes: \n\n %s\n", bytesOfMesssage,messageBuffer);
-        if(messageBuffer == NULL){
-            printf("repeat-->  %s\n", messageBuffer);
-        }
-        
+    // Error checking for getaddrinfo
+    if (gaiVal != 0 )
+    {
+        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(gaiVal));
+        return;
     }
 
-printf("size of list should be 1 and is: %d\n", List_count(messageList));
-List_add(&messageList, bytesOfMesssage);
-printf("size of list should be 1 and is: %d\n", List_count(messageList));
+    //crating socket + checks
+    for(p = servinfo; p!=NULL; p=p->ai_next)
+    {
+        sockRead = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+        // Error checking for socket
+        if (sockRead ==-1)
+        {
+            perror("receiver: socket() error");
+            continue;
+        }
 
-    //close socket// 
-    close(mysocket); 
 
+        // binding socket
+        bindVal = bind(sockRead, p->ai_addr, p->ai_addrlen);
+        // Error checking for bind
+        if(bindVal ==-1)
+        {
+            close(sockRead);
+            perror("receiver: bind() error in receiver");
+            continue;
+        }
+        break;
+    }
 
+     // Error checking if no sockets are binded
+    if(p==NULL)
+    {
+        fprintf(stderr, "receiver: failed to bind socket");
+        return;
+    }
 
+    // Freeing our results from getaddrinfo
+    freeaddrinfo(servinfo);
 
+    while(1)
+    {
+        // Receiving
+        messageLen = sizeof(their_addr);
+        write(sockRead, buf, MAXBUFLEN);
+
+        buf[readBytes]='\0'; // not sure what this does
+
+        // adding buff to list, 
+        message = (char*)malloc(sizeof(char)*(readBytes+1)); // allocates space for new item
+        strncpy(message, buf, readBytes);
+        message[readBytes] = '\0'; //not sure what this does
+
+        // Adding message to the front of list
+        List_prepend(list, message);
+    }
+
+}
+void receiverShutdown()
+{
+    close(sockRead);
 }
