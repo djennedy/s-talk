a4ff73e85d8c049c5d37e50548581ceca2e226da
Darren Jennedy <djennedy@sfu.ca>
2021-06-29 20:08:23 -0700

    Implemented main function

---
+++ launcher.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "list.h"
+#include "readInput.h"
+#include "writeOutput.h"
+#include "receiveUDP.h"
+#include "sendUDP.h"
+
+// Launches the s-talk program
+int main (int argc, char * argv[])
+{
+    // Checks if arguments are correct
+    if (argc!=4)
+    {
+        printf("Please enter valid arguments \n");
+        printf("Valid argument strucure is: \n");
+        printf("s-talk [my port number] [remote machine name] [remote port number]");
+        return -1;
+    }
+
+    // Storing arguments
+    char* myPort = argv[2];
+    char* theirHostname = argv[3];
+    char* theirPort = argv[4];
+
+    // Creates a shared list
+    List* list = List_create();
+
+    // Initializes the four modules
+    readerInit(list);
+    senderInit(theirHostname, theirPort, list);
+    receiverInit(myPort, list);
+    writerInit(list);
+
+    // TODO: Implement stop signal
+
+}
